52b3860b7115d2db326ee4ab4b874efa96e33350
simonkent <simon.kent@simonkent.com>
2014-03-27 15:33:13 +0000

    Added initial unit test for Grade

---
+++ src/test/scala/uk/ac/brunel/cs/fyp/model/GradeSpec.scala
@@ -0,0 +1,24 @@
+package uk.ac.brunel.cs.fyp.model
+
+import uk.ac.brunel.cs.fyp.model.Grade
+
+class GradeSpec extends UnitSpec {
+  
+	val validGrades = List("A*","A+","A","A-",
+					    "B+","B","B-",
+					    "C+","C","C-",
+					    "D+","D","D-",
+					    "E+","E","E-",
+					    "F")
+    val validGradePoints = List(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17).reverse
+    
+    
+    
+	"A grade" should "be created with a String between F and A* which correspond to gradepoints 1..17" in {
+	  assert(validGrades.map(g => new Grade(g)).map(g => g.gradePoint) == validGradePoints) 
+    }
+	
+	"A grade" should "also be created with an Int between 1..17 which corresponds to grades F..A*" in {
+	  assert(validGradePoints.map(gp => new Grade(gp)).map(g => g.grade) == validGrades) 
+    }
+}
---
+++ src/test/scala/uk/ac/brunel/cs/fyp/model/UnitSpec.scala
@@ -0,0 +1,6 @@
+package uk.ac.brunel.cs.fyp.model
+
+import org.scalatest._
+
+abstract class UnitSpec extends FlatSpec with Matchers with
+  OptionValues with Inside with Inspectors
